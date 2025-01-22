d6f359e1d2099009d0e0ff02c9d0d4c729fbf4b8
Carlos / <153391284+CarlosRW@users.noreply.github.com>
2025-01-22 10:53:40 -0600

    implement input clearing and game reset functionality

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 // Código de la aplicación
 let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
+console.log(numeroSecreto);
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector('h1');
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
     if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p', '¡Felicidades! Adivinaste el número secreto');
+        asignarTextoElemento('p', `¡Felicidades! Adivinaste el número secreto en ${intentos} ${(intentos === 1 ? 'intento' : 'intentos')}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
+        //El número no fue adivinado.
         if (numeroDeUsuario > numeroSecreto) {
             asignarTextoElemento('p', 'El número secreto es menor');
         } else {
             asignarTextoElemento('p', 'El número secreto es mayor');
         }
+        intentos++;
+        limpiar();
     }
     return;
 }
+
+function limpiar() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
 function generarNumeroSecreto() {
     return Math.floor(Math.random() * 10) + 1;
 }
