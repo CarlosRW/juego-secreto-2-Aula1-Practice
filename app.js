42d416330b276ad9314764ae1084786bd62a0b56
Carlos / <153391284+CarlosRW@users.noreply.github.com>
2025-01-22 01:51:00 -0600

    refactor game logic; implement secret number generation and user guess verification

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
+// Código de la aplicación
+let numeroSecreto = generarNumeroSecreto();
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Adivina el número secreto que está entre 1 y 10';
-
-function intentoDeUsuario() {
-  let numero = parseInt(prompt('Adivina el número secreto'));
-  return numero;
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector('h1');
+    elementoHTML.innerHTML = texto;
 }
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', '¡Felicidades! Adivinaste el número secreto');
+    } else {
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor');
+        } else {
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+    }
+    return;
+}
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Adivina el número secreto que está entre 1 y 10');
